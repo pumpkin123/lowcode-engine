316c87ed3467b37c2f8ba6014f7a5bc49a792b26
qnkong01 <qnkong01@mail.yst.com.cn>
2022-08-05 11:03:20 +0800

    feat(parse): 添加自定义类型

--- modules/material-parser/demo/parse-tsx.js
+++ modules/material-parser/demo/parse-tsx.js
@@ -1,11 +1,28 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: qnkong01
+ * @Date: 2022-07-27 13:15:41
+ * @LastEditors: qnkong01
+ * @LastEditTime: 2022-07-27 15:15:33
+ */
 const parse = require('../lib').default;
+const fs = require('fs');
 
 (async () => {
   const options = {
-    entry: './component.tsx',
+    entry: '../../../bpm-mobile/src/index.tsx',
     accesser: 'local',
   };
 
   const actual = await parse(options);
   console.log(JSON.stringify(actual, null, 2));
+  fs.writeFile('./demo/transfer.json', JSON.stringify(actual), err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //文件写入成功。
+  })
+  
 })();
